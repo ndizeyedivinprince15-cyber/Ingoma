8ea04cc4e6436c7992d9dd5397c655f0aeedfa24
prince <ndizeyedivinprince15@gmail.com>
2026-03-05 23:04:31 +0200

    fix: remove old users and profile modules

--- apps/api/src/modules/profile/dto/index.ts
+++
@@ -1,3 +0,0 @@
-// apps/api/src/modules/profile/dto/index.ts
-
-export { CreateUpdateProfileDto } from './create-update-profile.dto';
--- apps/api/src/modules/profile/index.ts
+++
@@ -1,4 +0,0 @@
-export { ProfileModule } from './profile.module';
-export { ProfileService } from './profile.service';
-export { ProfileController } from './profile.controller';
-export * from './dto';
